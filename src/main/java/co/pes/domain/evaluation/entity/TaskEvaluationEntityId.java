b306e19aa521bf44475c8f0e7386fa693650deeb
chanbinme <gksmfcksqls@gmail.com>
2024-11-06 18:30:08 +0900

    feat - JPA 마이그레이션: TaskEvaluationEntity 복합키 설정

--- src/main/java/co/pes/domain/evaluation/entity/TaskEvaluationEntityId.java
+++ src/main/java/co/pes/domain/evaluation/entity/TaskEvaluationEntityId.java
@@ -1,28 +1,20 @@
 package co.pes.domain.evaluation.entity;
 
-import co.pes.domain.member.entity.OrganizationEntity;
-import co.pes.domain.task.entity.TaskEntity;
 import java.io.Serializable;
 import javax.persistence.Embeddable;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
-@SuperBuilder
+@Builder
 @Embeddable
 @EqualsAndHashCode
 @NoArgsConstructor
+@AllArgsConstructor
 public class TaskEvaluationEntityId implements Serializable {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "task_id")
-    private TaskEntity task;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "charge_team_id")
-    private OrganizationEntity organization;
+    private Long taskId;
+    private Long chargeTeamId;
 
 }
